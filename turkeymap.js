c009320005b7f720fdebe550ec47a3ba8549cd9d
celinegungor <selingungor01@gmail.com>
2016-12-18 22:57:22 +0300

    SVG interactive aniamtion
    SVG interactive aniamtion

---
+++ turkeymap.js
@@ -0,0 +1,19 @@
+function svgturkiyeharitasi() {
+  $("#turkeymap path").hover(function() {
+    $(".names").html("<div> City name : " + $(this).parent().data("cityname") + " </br> Plate code : "+ $(this).parent().data("platecode") + 
+	" </br> Phone code : "+ $(this).parent().data("phonecode") +"</div>");
+    $(this).on("mousemove", function(event) {
+      $(".names").css("top", (event.pageY + 25));
+      $(".names").css("left", event.pageX);
+    });
+  },function(){
+    $(".names").html("");
+  });
+  $("#turkeymap path").on("click", function(event) {
+    var id = $(this).parent().attr("id");
+    var cityname = $(this).parent().data("cityname");
+    var platecode = $(this).parent().data("platecode");
+    var phonecode = $(this).parent().data("phonecode");
+    window.location = "#" + id + "-" + platecode;
+  });
+}
